68b6f2421976d7873f17e07c8ac935eb896194a9
Vladyslav Palyvoda <vladyslav_palyvoda@epam.com>
2022-11-14 23:47:58 +0200

    [Task 7] Authorization

---
+++ authorization-service/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    plugins: ['prettier'],
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: ['standard-with-typescript'],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.json',
+    },
+    rules: {
+        '@typescript-eslint/strict-boolean-expressions': [
+            2,
+            {
+                allowNullableBoolean: true,
+                allowNullableString: true,
+                allowNullableNumber: true,
+            },
+        ],
+        '@typescript-eslint/no-throw-literal': 0,
+        '@typescript-eslint/restrict-template-expressions': 0,
+    },
+};
